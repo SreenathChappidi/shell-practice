5794d9a30b9a5c9c1c52187b16ba81de0873bb55
sreenath chappidi <Sreenath.chappidi89@gmail.com>
2025-11-04 20:03:33 +0530

    some message

---
+++ 02-convesation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I am fine. How are you?"
+echo "Ramesh:: What are you learning recentely?"
+echo "Suresh:: I am learning shell scripting Ramesh"
