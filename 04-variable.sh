2e005ccbfc925e61e49b60278f86eb0620ab26e1
sreenath chappidi <Sreenath.chappidi89@gmail.com>
2025-11-04 20:32:20 +0530

    some message

---
+++ 04-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Welcome to Alkaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, finally not on zoom webinar"
+echo "$PERSON1:: So, do you have a deal"
+echo "Putin:: Yes, if you make Ukraine joins Russia"
